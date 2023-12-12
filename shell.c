59ff8f10ea0bf4b1973100cfc1bbb893f6588609
SaaW008 <saad.achnani@gmail.com>
2023-12-12 14:49:59 -0800

    update codes

--- shell.c
+++ shell.c
@@ -10,7 +10,7 @@
 int main(int argc, char *argv[])
 {
 	char *shell_prompt = "$ ", *line_break = "\n";
-	int return_status = 0, execution_status;
+	int rtrn_status = 0, execution_status;
 	int *execution_status_ptr = &execution_status;
 
 	program_name = argv[0];
@@ -23,15 +23,15 @@ int main(int argc, char *argv[])
 		exit(-100);
 	if (argc != 1)
 	{
-		return_status = cmd_process(argv[1], execution_status_ptr);
+		rtrn_status = cmd_process(argv[1], execution_status_ptr);
 		envir_free();
 		list_alias_free(alias_list);
 		return (*execution_status_ptr);
 	}
 	if (!isatty(STDIN_FILENO))
 	{
-		while (return_status != END_OF_FILE && return_status != EXIT)
-			return_status = process_args(execution_status_ptr);
+		while (rtrn_status != END_OF_FILE && rtrn_status != EXIT)
+			rtrn_status = process_args(execution_status_ptr);
 		envir_free();
 		list_alias_free(alias_list);
 		return (*execution_status_ptr);
@@ -39,10 +39,10 @@ int main(int argc, char *argv[])
 	while (1)
 	{
 		write(STDOUT_FILENO, shell_prompt, 2);
-		return_status = process_args(execution_status_ptr);
-		if (return_status == END_OF_FILE || return_status == EXIT)
+		rtrn_status = process_args(execution_status_ptr);
+		if (rtrn_status == END_OF_FILE || rtrn_status == EXIT)
 		{
-			if (return_status == END_OF_FILE)
+			if (rtrn_status == END_OF_FILE)
 				write(STDOUT_FILENO, line_break, 1);
 			envir_free();
 			list_alias_free(alias_list);
