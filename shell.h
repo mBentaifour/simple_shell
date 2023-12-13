d8bf29e2834d3167c18e57da781d9adef7e6f61a
SaaW008 <saad.achnani@gmail.com>
2023-12-13 15:12:14 -0800

    update codes

--- shell.h
+++ shell.h
@@ -16,14 +16,13 @@
 
 extern char **environ;
 int history_count;
-char *pgram_name;
+char *program_name;
 
 /**
- * struct builtin_s - str type defining builtin commands
- * @built_cmd_name: builtin cmd name
- * @fun: pointer to builtin command
-*/
-
+ * struct builtin_s - struct type defining builtin commands
+ * @built_cmd_name: builtin command name
+ * @fun: ptr to the builtin command
+ */
 typedef struct builtin_s
 {
 	char *built_cmd_name;
@@ -32,10 +31,9 @@ typedef struct builtin_s
 
 /**
  * struct list_s - struct type defining a linked list
- * @dir: the path of directory
- * @next: pointer to next node
-*/
-
+ * @dir: path of directory
+ * @next: ptr to next node
+ */
 typedef struct list_s
 {
 	char *dir;
@@ -46,9 +44,8 @@ typedef struct list_s
  * struct alias_s - struct defining aliases
  * @name: alias name
  * @value: alias value
- * @next: pointer to next node
+ * @next: ptr to the next node
  */
-
 typedef struct alias_s
 {
 	char *name;
@@ -69,47 +66,47 @@ char *_str_n_cat(char *dest_str, const char *src_str, size_t num);
 int _str_spn(char *str, char *pref);
 int _str_n_cmp(const char *str1, const char *str2, size_t num);
 
-char *fetch_camdda(char *camdda_line, int *last_camdda_status);
-int execute_cmd(char **camdda_args, char **start_of_args, int *last_camdda_status);
-int cmd_run(char **camdda_args, char **start_of_args, int *last_camdda_status);
-int process_args(int *last_camdda_status);
-int validate_args(char **camdda_args);
+char *fetch_cmd(char *cmd_line, int *last_cmd_status);
+int execute_cmd(char **cmd_args, char **start_of_args, int *last_cmd_status);
+int cmd_run(char **cmd_args, char **start_of_args, int *last_cmd_status);
+int process_args(int *last_cmd_status);
+int validate_args(char **cmd_args);
 
-void read_line(char **camdda, ssize_t mode);
-void rep_var(char **camdda_args, int *last_camdda_status);
-void args_free(char **last_camdda_status, char **start_of_args);
-char **substitute_alias(char **camdda_args);
+void read_line(char **cmd, ssize_t mode);
+void rep_var(char **cmd_args, int *last_cmd_status);
+void args_free(char **last_cmd_status, char **start_of_args);
+char **substitute_alias(char **cmd_args);
 
-char **custom_strtok(char *camdda, char *c_deli);
+char **custom_strtok(char *cmd, char *c_deli);
 void *custom_realloc(void *buff, unsigned int first_s, unsigned int second_s);
 ssize_t custom_getline(char **buff_ptr, size_t *buff_size, FILE *stream);
 char *custom_itoa(int n);
-char *fetch_loc(char *camdda);
-int execute(char **camdda_args, char **args_start);
+char *fetch_loc(char *cmd);
+int execute(char **command_args, char **args_start);
 list_t *fetch_dir_p(char *env_path);
 void free_list(list_t *head);
 
-int (*fetch_built(char *camdda))(char **camdda_args, char **start_of_args);
-int exit_hsh(char **camdda_args, char **start_of_args);
-int env_hsh(char **camdda_args, char __attribute__((__unused__)) **start_of_args);
-int setenv_hsh(char **camdda_args, char __attribute__((__unused__)) **start_of_args);
-int unsetenv_hsh(char **camdda_args, char __attribute__((__unused__)) **start_of_args);
-int cd_hsh(char **camdda_args, char __attribute__((__unused__)) **start_of_args);
-int alias_hsh(char **camdda_args, char __attribute__((__unused__)) **start_of_args);
-int help_hsh(char **camdda_args, char __attribute__((__unused__)) **start_of_args);
+int (*fetch_built(char *cmd))(char **cmd_args, char **start_of_args);
+int exit_hsh(char **cmd_args, char **start_of_args);
+int env_hsh(char **cmd_args, char __attribute__((__unused__)) **start_of_args);
+int setenv_hsh(char **cmd_args, char __attribute__((__unused__)) **start_of_args);
+int unsetenv_hsh(char **cmd_args, char __attribute__((__unused__)) **start_of_args);
+int cd_hsh(char **cmd_args, char __attribute__((__unused__)) **start_of_args);
+int alias_hsh(char **cmd_args, char __attribute__((__unused__)) **start_of_args);
+int help_hsh(char **cmd_args, char __attribute__((__unused__)) **start_of_args);
 
 char **custom_getenv(const char *var);
 void envir_free(void);
 char **_cpy_environement();
 
-char *sec_cd_err(char **camdda_args);
-char *sec_syn_err(char **camdda_args);
-char *ex_126_err(char **camdda_args);
-char *ex_127_err(char **camdda_args);
-int generate_err(char **camdda_args, int error);
-char *env_err(char **camdda_args);
-char *first_err(char **camdda_args);
-char *sec_ex_err(char **camdda_args);
+char *sec_cd_err(char **cmd_arg);
+char *sec_syn_err(char **cmd_arg);
+char *ex_126_err(char **cmd_arg);
+char *ex_127_err(char **cmd_arg);
+int generate_err(char **cmd_arg, int error);
+char *env_err(char **cmd_arg);
+char *first_err(char **cmd_arg);
+char *sec_ex_err(char **cmd_arg);
 
 alias_t *append_alias(alias_t **first_node, char *alias_name, char *alias_value);
 
@@ -127,5 +124,5 @@ void displ_all_info(void);
 void alias_displ_info(void);
 void cd_displ_info(void);
 
-int cmd_prcs(char *path_f, int *last_cmd_status);
+int cmd_process(char *path_f, int *last_cmd_status);
 #endif
