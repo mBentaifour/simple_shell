ba8cad5ef322eb6be2f33bbdc04977c2e6033ec3
SaaW008 <saad.achnani@gmail.com>
2023-12-13 15:16:59 -0800

    update codes

--- shell.c
+++ shell.c
@@ -1,20 +1,22 @@
 #include "shell.h"
 
 /**
- * main - principal function to execut shell
- * @argc: arguments count
- * @argv: an array of pointers to arguments
+ * main - main function of shell program
+ * @argc: number of arguments
+ * @argv: array of pointers to arguments
  *
- * Return: returned value from execution
+ * Return: returned from last executed command
  */
 int main(int argc, char *argv[])
 {
-	char *shell_prompt = "$ ", *line_break = "\n";
-	int rtrn_status = 0, execution_status;
+	char *shell_prompt = "$ ";
+	char *line_break = "\n";
+	int retu_status = 0;
+	int execution_status;
 	int *execution_status_ptr = &execution_status;
 
-	pgram_name = argv[0];
-	story_count = 1;
+	program_name = argv[0];
+	history_count = 1;
 	alias_list = NULL;
 	signal(SIGINT, sig_handler);
 	*execution_status_ptr = 0;
@@ -23,15 +25,15 @@ int main(int argc, char *argv[])
 		exit(-100);
 	if (argc != 1)
 	{
-		rtrn_status = camdda_process(argv[1], execution_status_ptr);
+		retu_status = cmd_process(argv[1], execution_status_ptr);
 		envir_free();
 		list_alias_free(alias_list);
 		return (*execution_status_ptr);
 	}
 	if (!isatty(STDIN_FILENO))
 	{
-		while (rtrn_status != END_OF_FILE && rtrn_status != EXIT)
-			rtrn_status = process_args(execution_status_ptr);
+		while (retu_status != END_OF_FILE && retu_status != EXIT)
+			retu_status = process_args(execution_status_ptr);
 		envir_free();
 		list_alias_free(alias_list);
 		return (*execution_status_ptr);
@@ -39,10 +41,10 @@ int main(int argc, char *argv[])
 	while (1)
 	{
 		write(STDOUT_FILENO, shell_prompt, 2);
-		rtrn_status = process_args(execution_status_ptr);
-		if (rtrn_status == END_OF_FILE || rtrn_status == EXIT)
+		retu_status = process_args(execution_status_ptr);
+		if (retu_status == END_OF_FILE || retu_status == EXIT)
 		{
-			if (rtrn_status == END_OF_FILE)
+			if (retu_status == END_OF_FILE)
 				write(STDOUT_FILENO, line_break, 1);
 			envir_free();
 			list_alias_free(alias_list);
